9a82afbc12dc229a76081ad1c05548a1e36448e5
ArpitArya29 <arpitaryan48@gmail.com>
2025-02-17 21:11:51 +0530

    Project calculator completed

---
+++ calculator/script.js
@@ -0,0 +1,32 @@
+const button = document.querySelector('.button_container');
+
+button.addEventListener('click',(e)=>{
+    
+    // checks if button is being clicked
+    if(e.target.nodeName==='BUTTON'){
+        
+        const value = e.target.innerText; // checking which button is clicked
+        const inputValue = document.getElementById('result_container');
+
+        switch(value){
+            case 'C':
+                inputValue.innerText = '';
+                break;
+
+            case '=':
+                const result = eval(inputValue.innerText);
+                inputValue.innerText = result;
+                break;
+
+            case 'DEL':
+                let val = inputValue.innerText;
+                inputValue.innerText = val.slice(0,val.length-1); // if it is a DEL button, we remove the string by slicing the string
+                break;
+
+            default:
+                const updatedValue = inputValue.innerText + value;
+                inputValue.innerText=updatedValue;
+        }
+
+    }
+})
